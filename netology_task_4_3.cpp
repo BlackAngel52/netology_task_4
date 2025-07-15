0fa960005a183e256d7fe3b69626af14fb76673e
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-15 21:48:58 +0300

    Add files via upload

---
+++ netology_task_4_3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int input{};
+    int count{1};
+
+    std::cout << "Enter an integer: " << std::endl;
+    std::cin >> input;
+
+    for (count; count <= 10; ++count) {
+        std::cout << input << " x " << count << " = " << input * count << std::endl;
+    }
+    return 0;
+}
+
+
