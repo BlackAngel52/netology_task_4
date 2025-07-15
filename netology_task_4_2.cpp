81bf7335f5eaedabab349c36f6136b4d74ca932c
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-15 21:32:58 +0300

    Add files via upload

---
+++ netology_task_4_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int input{};
+    int summ{};
+
+    std::cout << "Enter number: " << std::endl;
+    std::cin >> input;
+
+    while (input > 0) {
+        summ += input % 10;
+        input = input / 10;
+    }
+    std::cout << "Summ of digits = " << summ << std::endl;
+}
+
