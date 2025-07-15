dd7f7eed0809577c2a0f8a38bd236b15d63d305b
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-15 21:22:58 +0300

    Add files via upload

---
+++ netology_task_4_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    int count{};
+    int input{ 1 };
+    int summ{};
+    while (input != 0){
+        std::cout << "Enter number, or enter 0 to exit: " << std::endl;
+        std::cin >> input;
+        summ += input;
+    }
+    std::cout << "Summ = " << summ << std::endl;
+    
+}
